6063420857f9adcf322e0c32568b206b84583121
germaneduardocontacto-eng <germaneduardocontacto@gmail.com>
2026-09-23 14:45:05 -0300

    Update enviar.js

--- api/enviar.js
+++ api/enviar.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
   try {
     const data = await resend.emails.send({
       from: 'onboarding@resend.dev',
-      to: 'ggtecnologiaventas@gmail.com',
+      to: 'germaneduardocontacto@gmail.com',
       subject: 'Nueva Cotización de Mayorista',
       html: `<p>Nuevo mensaje de cotización:</p><p><b>Nombre:</b> ${nombre}</p><p><b>Email:</b> ${email}</p><p><b>Teléfono:</b> ${telefono}</p>`
     });
